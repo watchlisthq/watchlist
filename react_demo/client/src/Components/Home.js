1583c480860b816107943e4b2f4bc96ae2086893
MaximPodgore <56902053+MaximPodgore@users.noreply.github.com>
2023-03-05 14:06:31 -0800

    Merge genre

--- react_demo/client/src/Components/Home.js
+++ react_demo/client/src/Components/Home.js
@@ -1,8 +1,21 @@
+import { searchGenre } from "../api/find";
 import "./Home.css";
 
 import Slides from "./Slides"
 
+<<<<<<< Updated upstream
 export default function Home(props) {
+=======
+const result1 = await searchGenre("18");
+//onData(result1);
+const result2 = await searchGenre("35");
+//onData(result2);
+const result3 = await searchGenre("2");
+//onData(result3);
+
+
+export default function Home() {
+>>>>>>> Stashed changes
   return (
     <div>
       <div class="main__container">
@@ -12,11 +25,28 @@ export default function Home(props) {
         <div class="main__carousel">
           <Slides data={props.watchlist.data}/>
         </div>
-        <div class="recommended__title">
-          <span class="recommended">Recommended for you</span>
+        <div class="genre1-title">
+          <span class="genre1">Drama</span>
         </div>
+<<<<<<< Updated upstream
         <div class="recommended__carousel">
           <Slides data={props.watchlist.data}/>
+=======
+        <div class="genre1__carousel">
+          <Slides/>
+        </div>
+        <div class="genre2-title">
+          <span class="genre2">Comedy</span>
+        </div>
+        <div class="genre2__carousel">
+          <Slides/>
+        </div>
+        <div class="genre3-title">
+          <span class="genre3">Film Noir</span>
+        </div>
+        <div class="genre3__carousel">
+          <Slides/>
+>>>>>>> Stashed changes
         </div>
       </div>
     </div>
--- react_demo/client/src/api/find.js
+++ react_demo/client/src/api/find.js
@@ -17,8 +17,57 @@ export function searchTitle(title) {
   });
 }
 
+export function searchGenre(genre) {
+  const options = {
+    method: 'GET',
+    url: 'https://streaming-availability.p.rapidapi.com/v2/search/title',
+    params: {
+      country: 'us',
+      services: 'netflix,prime.buy,hulu.addon.hbo,peacock.free',
+      output_language: 'en',
+      show_type: 'movie',
+      genre: genre,
+      show_original_language: 'en',
+    },
+    headers: {
+      'X-RapidAPI-Key': 'f1da698a20mshfdaf67f61741fe5p12600ajsn29fb5424de6b',
+      'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+    }
+  };
+  return axios.request(options).then(function (response) {
+    return response.data.result;
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
 export function filterStreaming(result, service) {
   const filter = result.filter(show => show.streamingInfo.hasOwnProperty("us") && 
       show.streamingInfo.us.hasOwnProperty(service));
   return filter;
 }
+
+/**
+ * 1:"Biography"
+2:"Film Noir"
+3:"Game Show"
+4:"Musical"
+5:"Sport"
+6:"Short"
+7:"Adult"
+12:"Adventure"
+14:"Fantasy"
+16:"Animation"
+18:"Drama"
+27:"Horror"
+28:"Action"
+35:"Comedy"
+36:"History"
+37:"Western"
+53:"Thriller"
+80:"Crime"
+99:"Documentary"
+878:"Science Fiction"
+9648:"Mystery"
+10402:"Music"
+ */
