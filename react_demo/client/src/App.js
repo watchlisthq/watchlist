d2243e715bcbe88f616c07a2d53b14eb3a9194b5
Ray Heinonen <Ray_heinonen@hotmail.com>
2023-03-04 21:20:53 -0800

    frontend changes

--- react_demo/client/src/App.js
+++ react_demo/client/src/App.js
@@ -1,31 +1,17 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 
-import Home from "./Components/Home"
-import Navigation from "./Components/Navigation"
-import Results from "./Components/Results"
-
+import Home from "./Components/Home";
+import Navigation from "./Components/Navigation";
+import Results from "./Components/Results";
 
 function App() {
   const [data, setData] = useState(null);
   const [isSearching, setSearching] = useState(false);
 
-  document.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar__container");
-    const navbarHeight = 80;
-    const distanceFromTop = Math.abs(document.body.getBoundingClientRect().top);
-    if (distanceFromTop >= navbarHeight) navbar.classList.add("fixed-top");
-    else navbar.classList.remove("fixed-top");
-  });
-
   return (
     <div>
-      <Navigation onData={setData} onSearch={setSearching}/>
-      {!isSearching ? 
-      (
-        <Home/>
-      ) : (
-        <Results query={data}/>
-      )}
+      <Navigation onData={setData} onSearch={setSearching} />
+      {!isSearching ? <Home /> : <Results query={data} />}
     </div>
   );
 }
--- react_demo/client/src/Components/Results.js
+++ react_demo/client/src/Components/Results.js
@@ -3,14 +3,21 @@ import "./Results.css";
 export default function Results(props) {
   return (
     <div class="results__container">
-        {props.query.map(element => 
-          <Result 
-            image={element.backdropURLs.original} 
-            title={element.title} 
-            description={element.overview}>
-          </Result>)}
+      {props.query.map((element) => (
+        <Result
+          image={element.backdropURLs.original}
+          title={element.title}
+          description={element.overview}
+        ></Result>
+      ))}
     </div>
   );
+}
+
+function addTitle() {
+  var img = document.getElementById("buttonImage");
+  img.src = `{require("../images/check-title.png")}`;
+  return false;
 }
 
 function Result(props) {
@@ -18,10 +25,15 @@ function Result(props) {
     <div class="results__element">
       <div class="results__image">
         <img src={props.image} alt="poster" width="331" height="auto"></img>
-        </div>
+      </div>
       <div class="results__inner">
-        <div class="results__title">{props.title}</div>
-        <div class="results__description">{props.description}</div>
+        <div class="title__inner">
+          <div class="results__title">{props.title}</div>
+          <div class="results__description">{props.description}</div>
+        </div>
+        <button id="buttonImage" onclick="addTitle()">
+          <img id="buttonImage" src={require("../images/add-title.png")}></img>
+        </button>
       </div>
     </div>
   );
