24a7c5cec186035f77b7ebbdb0b9c1d5e935a855
MaximPodgore <56902053+MaximPodgore@users.noreply.github.com>
2023-03-05 20:18:13 -0800

    Working on genres

--- react_demo/client/src/App.js
+++ react_demo/client/src/App.js
@@ -6,10 +6,14 @@ import Navigation from "./Components/Navigation";
 import Results from "./Components/Results";
 
 import { readData } from "./api/save";
+import { readGenre1 } from "./Components/Home";
 
 export default function App() {
   const [save, setSave] = useState("");
   const [data, setData] = useState(null);
+  const [genre1, setGenre1] = useState(true);
+  const [genre2, setGenre2] = useState(true);
+  const [genre3, setGenre3] = useState(true);
   const [isSearching, setSearching] = useState(false);
 
   useEffect(() => {
@@ -20,10 +24,19 @@ export default function App() {
     loadSave();
   }, [])
 
+  useEffect(() => {
+    async function loadGenre1() {
+      setGenre1(await readGenre1());
+    }
+    console.log("fired");
+    loadGenre1();
+  }, [])
+
   return (
     <div>
       <Navigation onData={setData} onSearch={setSearching} />
-      {!isSearching ? <Home watchlist={save}/> : <Results query={data} />}
+      {!isSearching ? <Home watchlist={save} Home genre1 = {genre1}/> 
+      : <Results query={data} />}
     </div>
   );
 }
--- react_demo/client/src/Components/Home.js
+++ react_demo/client/src/Components/Home.js
@@ -3,19 +3,15 @@ import "./Home.css";
 
 import Slides from "./Slides"
 
-<<<<<<< Updated upstream
+export async function readGenre1() {
+  let fetchData = async() => {
+    let result = await searchGenre("18");
+    return result
+  }
+  return await fetchData();
+}
+
 export default function Home(props) {
-=======
-const result1 = await searchGenre("18");
-//onData(result1);
-const result2 = await searchGenre("35");
-//onData(result2);
-const result3 = await searchGenre("2");
-//onData(result3);
-
-
-export default function Home() {
->>>>>>> Stashed changes
   return (
     <div>
       <div class="main__container">
@@ -28,25 +24,8 @@ export default function Home() {
         <div class="genre1-title">
           <span class="genre1">Drama</span>
         </div>
-<<<<<<< Updated upstream
         <div class="recommended__carousel">
-          <Slides data={props.watchlist.data}/>
-=======
-        <div class="genre1__carousel">
-          <Slides/>
-        </div>
-        <div class="genre2-title">
-          <span class="genre2">Comedy</span>
-        </div>
-        <div class="genre2__carousel">
-          <Slides/>
-        </div>
-        <div class="genre3-title">
-          <span class="genre3">Film Noir</span>
-        </div>
-        <div class="genre3__carousel">
-          <Slides/>
->>>>>>> Stashed changes
+          <Slides data={props.genre1.data}/>
         </div>
       </div>
     </div>
